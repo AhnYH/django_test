0f716daaa92a8b4443141bb9794a3acc89e4f6d8
ahnyh <ahnyh27@kotech.co.kr>
2018-08-30 17:58:28 +0900

    테스트 등록

---
+++ main/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from .forms import LoginForm
+import sys
+import os
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login as dlogin
+from django.http import JsonResponse
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+@csrf_exempt
+def login(request):
+    form = LoginForm(request.POST or None)
+    aaaaa = form.is_valid()
+    if request.method == 'POST' and form.is_valid():
+        user = form.authenticate_user()
+        dlogin(request, user)
+        sign = request.session
+
+        return render(request, 'signin.html')
+
+
+@csrf_exempt
+def signin(request):
+    return render(request, 'signin.html')
+
+
+@csrf_exempt
+def req_test(request):
+
+    return JsonResponse({'msg': 'connect'})
